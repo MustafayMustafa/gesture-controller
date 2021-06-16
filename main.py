f342c64ec6becd22e63dfb4fa1e260d0d736f43a
mustafa <mustafaymustafa@icloud.com>
2021-06-17 00:22:12 +0100

    uncomment image flip and conversion

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ with mp_hands.Hands(
 
     # Flip the image horizontally for a later selfie-view display, and convert
     # the BGR image to RGB.
-    # image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+    image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
     # To improve performance, optionally mark the image as not writeable to
     # pass by reference.
     image.flags.writeable = False
