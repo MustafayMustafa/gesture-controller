1c634f48e4621fac1c32e9f15e452b3b27590cc4
mustafa <mustafaymustafa@icloud.com>
2021-06-19 18:09:57 +0100

    black

--- tracking.py
+++ tracking.py
@@ -17,13 +17,13 @@ debug = True
 while cap.isOpened():
     success, image = cap.read()
     if not success:
-      print("Ignoring empty camera frame.")
-      # If loading a video, use 'break' instead of 'continue'.
-      continue
+        print("Ignoring empty camera frame.")
+        # If loading a video, use 'break' instead of 'continue'.
+        continue
 
     image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
     image.flags.writeable = False
-    results = hands.process(image)    
+    results = hands.process(image)
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
@@ -33,27 +33,34 @@ while cap.isOpened():
             if debug:
                 for id, land_mark in enumerate(hand_landmarks.landmark):
                     h, w, c = image.shape
-                    cx, cy = int(land_mark.x*w), int(land_mark.y*h)
+                    cx, cy = int(land_mark.x * w), int(land_mark.y * h)
                     print(f"ID: {id}, x: {cx}, y: {cy}")
 
-            mp_drawing.draw_landmarks(
-            image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+            mp_drawing.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
 
     # calculate, draw fps
     current_time = time.time()
-    fps = 1/(current_time-last_time)
+    fps = 1 / (current_time - last_time)
     last_time = current_time
     cv2.putText(
-        img=image, 
-        text=str(int(fps)), 
-        org=(20, 70), 
-        fontFace=cv2.FONT_HERSHEY_PLAIN, 
-        fontScale=5, 
-        color=(255, 255, 255), 
-        thickness=1
+        img=image,
+        text=str(int(fps)),
+        org=(20, 70),
+        fontFace=cv2.FONT_HERSHEY_PLAIN,
+        fontScale=5,
+        color=(255, 255, 255),
+        thickness=1,
     )
 
-    cv2.imshow('Image', image)
+    cv2.imshow("Image", image)
     if cv2.waitKey(5) & 0xFF == 27:
-      break
+        break
 cap.release()
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
